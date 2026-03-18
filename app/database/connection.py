6d02389b4801385ff856eeb82511d0e82731f015
Nicolas C.S <117557162+MrZLeviatan@users.noreply.github.com>
2026-03-17 20:38:39 -0500

    Implementación de Test en la base de datos locales
    Se crea el test para la base de datos local ( SQLite ), para la automatización de los datos.

--- app/database/connection.py
+++ app/database/connection.py
@@ -5,6 +5,7 @@ Objetivos:
     - Mantener la portabilidad entre SQLite y PostgreSQL sin cambios en
         el código de la aplicación.
 """
+import os
 
 # Función central para crear el motor de conexión.
 from sqlalchemy import create_engine
@@ -15,12 +16,22 @@ from sqlalchemy.orm import sessionmaker
 # Importa configuración centralizada.
 from app.config import DATABASE_URL
 
+# Asegurar que el directorio de SQLite existe
+if DATABASE_URL.startswith("sqlite:///"):
+    db_path = DATABASE_URL.replace("sqlite:///", "")
+    db_dir = os.path.dirname(db_path)
+
+    if db_dir and not os.path.exists(db_dir):
+        os.makedirs(db_dir, exist_ok=True)
+
+
 # El engine es la interfaz principal con la base de datos.
 # Gestiona conexiones y ejecución de SQL.
 engine = create_engine(
     DATABASE_URL,
     echo=False  # Deshabilitar el registro de SQL en la consola
 )
+
 
 # SessionLocal es una fábrica de sesiones.
 # Cada sesión representa una transacción independiente.
